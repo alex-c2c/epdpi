73cae5e6c2b542e90e71894fd445665f0838928e
Alex Lim <kaitwee@gmail.com>
2025-04-08 12:15:20 +0800

    update

--- src/main.py
+++ src/main.py
@@ -42,6 +42,29 @@ font_full_2 = ImageFont.truetype(os.path.join("font", "Roboto-Bold.ttf"), 250)
 font_full_3 = ImageFont.truetype(os.path.join("font", "Roboto-Bold.ttf"), 300)
 
 
+def redis_event_handler(msg: dict[str, str]) -> None:
+    if msg["type"] != "message" or msg["channel"] != CHANNEL_EPDPI:
+        return
+
+    data: list[str] = msg["data"].split("^")
+
+    if data[0] == MSG_CLEAR:
+        clear_display()
+
+    elif data[0] == MSG_DRAW:
+        file_path: str = data[1]
+        time: str = data[2]
+        mode: TimeMode = TimeMode(int(data[3]))
+        color: int = int(data[4])
+        shadow: int = int(data[5])
+        draw_grids: bool = True if data[6] == "1" else False
+
+        if file_path == "":
+            draw_time(time, mode, color, shadow, draw_grids)
+        else:
+            draw_image_with_time(file_path, time, mode, color, shadow, draw_grids)
+            
+
 def is_machine_valid() -> bool:
     return "IS_RASPBERRYPI" in os.environ
 
--- src/redis_controller.py
+++ src/redis_controller.py
@@ -1,30 +1,7 @@
 import redis
 import logging
-from .main import clear_display, draw_image_with_time, draw_time
-from .consts import *
-
-
-def redis_event_handler(msg: dict[str, str]) -> None:
-    if msg["type"] != "message" or msg["channel"] != CHANNEL_EPDPI:
-        return
-
-    data: list[str] = msg["data"].split("^")
-
-    if data[0] == MSG_CLEAR:
-        clear_display()
-
-    elif data[0] == MSG_DRAW:
-        file_path: str = data[1]
-        time: str = data[2]
-        mode: TimeMode = TimeMode(int(data[3]))
-        color: int = int(data[4])
-        shadow: int = int(data[5])
-        draw_grids: bool = True if data[6] == "1" else False
-
-        if file_path == "":
-            draw_time(time, mode, color, shadow, draw_grids)
-        else:
-            draw_image_with_time(file_path, time, mode, color, shadow, draw_grids)
+from main import redis_event_handler
+from consts import *
 
 
 def redis_exception_handler(ex, pubsub, thread) -> None:
