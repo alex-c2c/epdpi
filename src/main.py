64ad2188613a1685dc91dc9cbda690d28ea9afe8
Alex Lim <kaitwee@gmail.com>
2025-04-08 15:11:29 +0800

    update

--- src/main.py
+++ src/main.py
@@ -130,8 +130,7 @@ def btn_cb_next_img() -> None:
     
     global redis_client
     redis_client.publish("clockpi", "button^next")
-    set_epd_busy(True)
-    set_epd_busy(False)
+    redis_publish("test", "1", "2", "3", 4)
 
 
 def btn_cb_prev_img() -> None:
