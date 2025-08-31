db6f1eea26b30b60a288f4f00a1ed479ea563f0d
Alex Lim <kaitwee@gmail.com>
2025-08-31 22:17:06 +0800

    fix double quotes in string

--- src/consts.py
+++ src/consts.py
@@ -6,8 +6,8 @@ REDIS
 R_CH_PUB: str = f"clockpi"
 R_CH_SUB: str = f"epdpi_{os.getenv('ID')}"
 
-R_CH_DRAW: str = f"epdpi_draw_{os.getenv("ID")}"
-R_CH_CLEAR: str = f"epdpi_clear_{os.getenv("ID")}"
+R_CH_DRAW: str = f"epdpi_draw_{os.getenv('ID')}"
+R_CH_CLEAR: str = f"epdpi_clear_{os.getenv('ID')}"
 
 R_MSG_CLEAR: str = "clear"
 R_MSG_DRAW: str = "draw"
